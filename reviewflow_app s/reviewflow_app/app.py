eaab3851fb7f8eea4e736584a2a51d13fad4151b
Sudhanshu <sudubhai555@gmail.com>
2026-06-25 12:58:53 +0530

    Update app.py

--- reviewflow_app s/reviewflow_app/app.py
+++ reviewflow_app s/reviewflow_app/app.py
@@ -600,4 +600,5 @@ def serve_qr(filename):
     return send_from_directory('static/qr_codes', filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
